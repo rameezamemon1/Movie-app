b6ae45c639966dc839a79b462efb4f458fa5629b
rameezamem1 <rameeza.memon1@gmail.com>
2022-08-19 12:44:05 +0500

    git cache cleared

--- server.js
+++ server.js
@@ -1,26 +1,19 @@
 const dotenv = require("dotenv");
-const app = require("./app");const path = require("path");
-
+const app = require("./app");
 const mongoose = require("mongoose");
 const express = require("express");
 
 dotenv.config({ path: "./config.env" });
 
-const DB = process.env.DATABASE;
+const DB = process.env.DATABASE
+
+app.use(express.static("public"))
 
 mongoose.connect(DB, { useUnifiedTopology: true }).then(() => {
   console.log("DB connection is established!");
 });
 
-// console.log(__dirname + '/public/index.html')
-// app.get('/', function (req, res) {
-
-//   res.sendFile(__dirname + '/public/index.html')
-
-// })
-app.use('/static', express.static(path.join(__dirname, 'public')))
-
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
